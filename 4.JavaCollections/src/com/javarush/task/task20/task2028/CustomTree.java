02316426161bc1eea1b81abe31df0c076f5a1562
Dmitry <neustupov@yandex.ru>
2017-08-02 20:39:02 +0300

    edit task2028

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -1,17 +1,216 @@
 package com.javarush.task.task20.task2028;
 
-import java.io.Serializable;
-import java.util.*;
-import java.util.concurrent.ConcurrentLinkedQueue;
+        import java.io.Serializable;
+        import java.util.*;
+        import java.util.function.Consumer;
+        import java.util.function.Predicate;
+        import java.util.function.UnaryOperator;
+        import java.util.stream.Stream;
 
 /*
 Построй дерево(1)
 */
-public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
-    Entry<String> root = new Entry<>("");
+public class CustomTree extends AbstractList<String> implements Cloneable, Serializable{
+    Entry<String> root = new Entry<>(null);
+
+    public static void main(String[] args) {
+        List<String> list = new CustomTree();
+        for (int i = 1; i <= 16; i++) {
+            list.add(String.valueOf(i));
+        }
+        System.out.println("Size: " + ((CustomTree) list).size());
+        list.remove("2");
+        System.out.println("Size: " + ((CustomTree) list).size());
+
+        System.out.println("Expected 3, actual is " + ((CustomTree) list).getParent("8"));
+        //list.remove("5");
+        System.out.println("Expected null, actual is " + ((CustomTree) list).getParent("11"));
+    }
+
+    @Override
+    public boolean add(String s) {
+        if (s == null)
+            return false;
+        Entry<String> rootElement;
+        Queue<Entry<String>> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            rootElement = queue.poll();
+            rootElement.checkChildren();
+            if(rootElement.availableToAddLeftChildren){
+                rootElement.leftChild = new Entry<>(s);
+
+                //System.out.println(rootElement.leftChild.elementName);
+                return true;
+            }
+            else
+                queue.offer(rootElement.leftChild);
+
+            if(rootElement.availableToAddRightChildren){
+                rootElement.rightChild = new Entry<>(s);
+                //System.out.println(rootElement.rightChild.elementName);
+                return true;
+            }
+            else
+                queue.offer(rootElement.rightChild);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (o == null)
+            return false;
+
+        String s = (String) o;
+
+        Entry<String> rootElement;
+        Queue<Entry<String>> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            rootElement = queue.poll();
+            rootElement.checkChildren();
+            if(!rootElement.availableToAddLeftChildren) {
+                if (rootElement.leftChild.elementName.equals(s)) {
+                    rootElement.leftChild = null;
+                    return true;
+                } else
+                    queue.offer(rootElement.leftChild);
+            }
+
+            if(!rootElement.availableToAddRightChildren) {
+                if (rootElement.rightChild.elementName.equals(s)) {
+                    rootElement.rightChild = null;
+                    return true;
+                } else
+                    queue.offer(rootElement.rightChild);
+            }
+        }
+        return false;
+    }
+
+    public String getParent(String s) {
+        if (root == null || s == null || s.equals(root.elementName))
+            return null;
+
+        Entry<String> rootElement;
+        Queue<Entry<String>> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            rootElement = queue.poll();
+            rootElement.checkChildren();
+            if(rootElement.leftChild != null) {
+                if (rootElement.leftChild.elementName.equals(s)) {
+                    return rootElement.elementName;
+                } else
+                    queue.offer(rootElement.leftChild);
+            }
+
+            if(rootElement.rightChild != null) {
+                if (rootElement.rightChild.elementName.equals(s)) {
+                    return rootElement.elementName;
+                } else
+                    queue.offer(rootElement.rightChild);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int size() {
+        Entry<String> rootElement;
+        Queue<Entry<String>> queue = new LinkedList<>();
+
+        queue.offer(root);
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            rootElement = queue.poll();
+            rootElement.checkChildren();
+            if(rootElement.leftChild != null) {
+                queue.offer(rootElement.leftChild);
+                count++;
+            }
+
+            if(rootElement.rightChild != null) {
+                queue.offer(rootElement.rightChild);
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+
+    static class Entry<T> implements Serializable{
+        String elementName;
+        int lineNumber;
+        boolean availableToAddLeftChildren, availableToAddRightChildren;
+        Entry<T> parent, leftChild, rightChild;
+
+        public Entry(String elementName) {
+            this.elementName = elementName;
+            this.availableToAddLeftChildren = true;
+            this.availableToAddRightChildren = true;
+        }
+
+        void checkChildren(){
+            //availableToAddLeftChildren = leftChild != null ? false : true;
+            //availableToAddRightChildren = rightChild != null ? false : true;
+
+            if(leftChild != null)
+                availableToAddLeftChildren = false;
+            if(rightChild != null)
+                availableToAddRightChildren = false;
+        }
+
+        boolean isAvailableToAddChildren(){
+            return availableToAddLeftChildren | availableToAddRightChildren;
+        }
+    }
 
     @Override
     public String get(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void replaceAll(UnaryOperator operator) {
+
+    }
+
+    @Override
+    public void sort(Comparator c) {
+
+    }
+
+    @Override
+    public Spliterator spliterator() {
+        return null;
+    }
+
+    @Override
+    public boolean removeIf(Predicate filter) {
+        return false;
+    }
+
+    @Override
+    public Stream stream() {
+        return null;
+    }
+
+    @Override
+    public Stream parallelStream() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer action) {
+
+    }
+
+    @Override
+    public void add(int index, String element) {
         throw new UnsupportedOperationException();
     }
 
@@ -21,7 +220,7 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
     }
 
     @Override
-    public void add(int index, String element) {
+    public String remove(int index) {
         throw new UnsupportedOperationException();
     }
 
@@ -31,149 +230,12 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
     }
 
     @Override
-    protected void removeRange(int fromIndex, int toIndex) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
     public boolean addAll(int index, Collection<? extends String> c) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public String remove(int index) {
+    protected void removeRange(int fromIndex, int toIndex) {
         throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean add(String newElement) {
-        Queue<Entry<String>> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            Entry<String> currentNode = queue.poll();
-            currentNode.checkChildren();
-            if (currentNode.isAvailableToAddChildren()) {
-                if (currentNode.availableToAddLeftChildren) {
-                    currentNode.leftChild = new Entry<>(newElement);
-                    currentNode.leftChild.parent = currentNode;
-                    return true;
-                } else if (currentNode.availableToAddRightChildren) {
-                    currentNode.rightChild = new Entry<>(newElement);
-                    currentNode.rightChild.parent = currentNode;
-                    return true;
-                }
-            } else {
-                if (currentNode.leftChild != null) {
-                    queue.offer(currentNode.leftChild);
-                }
-                if (currentNode.rightChild != null) {
-                    queue.offer(currentNode.rightChild);
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean remove(String s) {
-        Queue<Entry<String>> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            Entry<String> currentNode = queue.poll();
-            if (currentNode.leftChild != null) {
-                if (currentNode.leftChild.elementName.equals(s)) {
-                    currentNode.leftChild = null;
-                    return true;
-                }
-                queue.offer(currentNode.leftChild);
-            }
-            if (currentNode.rightChild != null) {
-                if (currentNode.rightChild.elementName.equals(s)) {
-                    currentNode.rightChild = null;
-                    return true;
-                }
-                queue.offer(currentNode.rightChild);
-            }
-        }
-        return false;
-    }
-
-    String getParent(String entryName) {
-        Queue<Entry<String>> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            Entry<String> currentNode = queue.poll();
-            if (currentNode.elementName.equals(entryName)) {
-                return currentNode.parent.elementName;
-            } else {
-                if (currentNode.leftChild != null) {
-                    queue.offer(currentNode.leftChild);
-                }
-                if (currentNode.rightChild != null) {
-                    queue.offer(currentNode.rightChild);
-                }
-            }
-        }
-        return null;
-    }
-
-    static class Entry<T> implements Serializable {
-        String elementName;
-        int lineNumber;
-        boolean availableToAddLeftChildren, availableToAddRightChildren;
-        Entry<T> parent, leftChild, rightChild;
-
-        public Entry(String name) {
-            this.elementName = name;
-            this.availableToAddLeftChildren = true;
-            this.availableToAddRightChildren = true;
-        }
-
-        void checkChildren() {
-            if (leftChild != null) {
-                this.availableToAddLeftChildren = false;
-            } else {
-                this.availableToAddLeftChildren = true;
-            }
-            if (rightChild != null) {
-                this.availableToAddRightChildren = false;
-            } else {
-                this.availableToAddRightChildren = true;
-            }
-        }
-
-        boolean isAvailableToAddChildren() {
-            return (availableToAddLeftChildren || availableToAddRightChildren);
-        }
-    }
-
-    @Override
-    public int size() {
-        int size = -1;
-        Queue<Entry<String>> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            Entry<String> currentNode = queue.poll();
-            size++;
-            if (currentNode.leftChild != null) {
-                queue.offer(currentNode.leftChild);
-            }
-            if (currentNode.rightChild != null) {
-                queue.offer(currentNode.rightChild);
-            }
-        }
-        return size;
-    }
-
-    public static void main(String[] args) {
-        List<String> list = new CustomTree();
-        for (int i = 1; i < 23; i++) {
-            list.add(String.valueOf(i));
-        }
-        System.out.println("Expected 3, actual is " + ((CustomTree) list).getParent("8"));
-        System.out.println(list.size());
-//        ((CustomTree) list).remove("15");
-//        System.out.println(list.size());
-        ((CustomTree) list).remove("2");
-        System.out.println(list.size());
     }
 }
